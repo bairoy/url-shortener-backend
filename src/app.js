6b7594aaae9bd96d1aada7fd1fdd39b23acb0986
bairoy <luckyroy0110lafra@gmail.com>
2026-03-27 22:27:39 +0530

    fixed cors origin

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:"https://url-shortener-frontend-baiju.vercel.app/"
+}));
 app.use(express.json());
 app.use("/", urlRoutes);
 
